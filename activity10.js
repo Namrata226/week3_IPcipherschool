a89f2fb4b3260fd6c5a7ef85958a1548d571398a
Namrata226 <119877792+Namrata226@users.noreply.github.com>
2022-12-29 23:50:06 +0530

    Create activity10.js

---
+++ activity10.js
@@ -0,0 +1,30 @@
+//
+
+let attribute = document.querySelector("img").attributes;
+console.log(attribute);
+let hasattribute = document.querySelector("img").hasAttribute("src");
+console.log(hasattribute);
+let getattribute = document.querySelector("img").getAttribute("border");
+console.log(getattribute);
+let setattribute = document
+  .querySelector("img")
+  .setAttribute("src", "/assets/1-plant.png");
+console.log(setattribute);
+let removeattribute = document
+  .querySelector("img")
+  .setAttribute("src", "/assets/1-plant.png");
+console.log(removeattribute);
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
