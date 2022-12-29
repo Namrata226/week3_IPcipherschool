a8bff18ebbdc55946a56505ed1b568d118023fa8
Namrata226 <119877792+Namrata226@users.noreply.github.com>
2022-12-29 23:55:46 +0530

    Create activity14.js

---
+++ activity14.js
@@ -0,0 +1,28 @@
+// Single Line comment
+
+/*Multi 
+line
+comment*/
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navlinks = document.querySelectorAll(".nav-links");
+  burger.addEventListener("click",() =>{
+  //Toggle Nav
+  nav.classList.toggle("nav-active");)}
+};
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
