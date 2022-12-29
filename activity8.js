e21b004a8d72009d96b092ea7738037d3de4e154
Namrata226 <119877792+Namrata226@users.noreply.github.com>
2022-12-29 23:47:37 +0530

    Create activity8.js

---
+++ activity8.js
@@ -0,0 +1,48 @@
+// string
+let string = "Text";
+console.log("string:", string);
+
+// integer
+let integer = 13;
+console.log("number/integer:", integer);
+
+// float value
+let float = 3.8;
+console.log("float value:", float);
+
+// boolean value
+let boolean = true;
+console.log("Boolean:", boolean);
+
+// null value
+let something = null;
+console.log(something);
+
+// undefined
+let undefinedAssign;
+console.log("Undefined:", undefined);
+
+// object
+const object = {
+  class: "Twelveth",
+  number: 50,
+};
+
+console.log("Object Data", typeof object);
+// array
+const array = ["laptop", "Diary", "Pens", "Coffee", "Mango"];
+console.log("array:", array);
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
