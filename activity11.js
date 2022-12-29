7785236a44b27157da34f2c980ff14db2eb5b83a
Namrata226 <119877792+Namrata226@users.noreply.github.com>
2022-12-29 23:52:53 +0530

    Create activity11.js

---
+++ activity11.js
@@ -0,0 +1,9 @@
+// Modifying Inline Style in JS
+
+let footer = document.querySelector("footer").style;
+console.log(footer);
+// let footerstyle = (document.querySelector("footer").style.color = "purple");
+let footerstyle = (document.querySelector("footer").style.backgroundColor =
+  "purple");
+Footer
+© 2022 GitHu
